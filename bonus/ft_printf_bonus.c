f8e2e5e904ea284285f90aabf14da60221545685
Busra Seven <busras3v3n@proton.me>
2025-02-28 15:51:46 +0300

    added bonus

---
+++ bonus/ft_printf_bonus.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "./ft_printf/ft_printf.h"
+
+int main()
+{
+	int a = 4;
+	printf("%       -.9d\n", 9);
+}
---
+++ bonus/ft_printf_bonus.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_bonus.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 11:13:48 by busseven          #+#    #+#             */
+/*   Updated: 2025/02/28 15:36:58 by busseven         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+typedef struct	s_flags
+{
+	int	left_justify;
+	int	space_sign;
+	int	hash;
+	int	padding_type;
+	int padding_place;
+	int	padding_number;
+	int	precision_place;
+	int	precision_number;
+	int	var_type;
+	int	len;
+} t_flags;
+
+int	ft_printf(const char *s, ...);
+int	ft_print_char(char c);
+int	ft_print_str(char *s);
+int	ft_print_nbr(int i);
+int	ft_print_hex(unsigned int i, char s);
+int	ft_print_ptr(void *p);
+int	ft_print_unbr(unsigned int n, int fd);
+int	ft_print_percent(void);
+
+#endif
