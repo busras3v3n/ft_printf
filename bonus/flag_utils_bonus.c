0ac9112ba2b02a9cea20c5deda618fd2d48627f1
Busra Seven <busras3v3n@proton.me>
2025-03-12 16:57:35 +0300

    fixed infinite loop error with plus flag

--- bonus/flag_utils_bonus.c
+++ bonus/flag_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 19:44:19 by busseven          #+#    #+#             */
-/*   Updated: 2025/03/12 12:58:40 by busseven         ###   ########.fr       */
+/*   Updated: 2025/03/12 16:57:02 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,7 +76,7 @@ void	check_precision(t_flags *flags, const char *s, va_list *args)
 			i++;
 		pre_str = ft_substr(ptr, 0, i);
 		flags->precision = ft_atoi(pre_str);
-		flags->i = i + ft_strlen(pre_str) + 3;
+		flags->i = i + ft_strlen(pre_str) + 4;
 		free(pre_str);
 	}
 	else if (!is_in_str(s[flags->i], flags->valid))
